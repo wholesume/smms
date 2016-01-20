808f9b38034fcecb499249c71df47d7b3ac18dfc
schweinsteiger <trapp.marlon@gmail.com>
2016-01-20 13:57:12 -0200

    Login working

--- www/js/calls_service.js
+++ www/js/calls_service.js
@@ -7,7 +7,9 @@ var serviceUrl = "http://calls.uptitek.com/calls/api/"
 var service = "call/"; // The entry url to the remote application api
 var clientKey = "59a94a0ac0f75200d1477d0f158a23d7feb08a2db16d21233b36fc8fda1a958c1be52b439f7957733bd65950cdfa7918b2f76a480ed01bb6e4edf4614eb8a708";
 //Super user
-var userKey = "e9f809e730f97cdbe4724e5bd6acc9b2165dcf7408bb3c733de534214e61f691a087eba7c674f1aa38cf5631a3c3c48e29194a3113b65756b0bbfdf98f7964a5";
+//var userKey = "e9f809e730f97cdbe4724e5bd6acc9b2165dcf7408bb3c733de534214e61f691a087eba7c674f1aa38cf5631a3c3c48e29194a3113b65756b0bbfdf98f7964a5";
+
+var userKey;
 
 //Admin user
 //var userKey = "507b553b106b1b9963b7affb34e5ed14bc1160bbdea24c094405b306bdcb2520823a0c7db7da4b51cf45cbdbad519eeca9affd7103b131d1e65a4974ba56b18d"
@@ -19,15 +21,15 @@ var userKey = "e9f809e730f97cdbe4724e5bd6acc9b2165dcf7408bb3c733de534214e61f691a
 //    });
 //});
 
-smmsCallService.factory('CallService', function ($http) {
+smmsCallService.factory('CallService', function ($http, SettingsService) {
 
     return {
         getCalls: function () {
-            return $http.get(serviceUrl + service + 'list/' + clientKey + '/' + userKey);
+            return $http.get(serviceUrl + service + 'list/' + clientKey + '/' + SettingsService.getUserToken());
         },
 
         getCall: function ($stateParams) {
-            return $http.get(serviceUrl + service + $stateParams.itemId + '/' + clientKey + '/' + userKey);
+            return $http.get(serviceUrl + service + $stateParams.itemId + '/' + clientKey + '/' + SettingsService.getUserToken());
         }
     }
 });
@@ -41,15 +43,42 @@ smmsCallService.factory('LoginService', function ($http) {
                 },
                 // Work-around to avoid the default transformation to json.
                 // To remove it we have to change the return type from server to plain text.
-                transformResponse: function(data, headers){
+                transformResponse: function (data, headers) {
                     return data;
                 }
             };
             return $http.post(serviceUrl + 'users/userRole/' + clientKey, user, config);
         },
-        
+
         getUser: function (user) {
             return $http.post(serviceUrl + 'users/auth/' + clientKey, user);
         }
+    }
+});
+
+smmsCallService.factory('SettingsService', function () {
+
+    function _retrieveValue(key) {
+        var value = localStorage[key];
+        if (value) {
+            return value;
+        }
+        return null;
+    }
+
+    function _saveValue(key, value) {
+        localStorage[key] = value;
+    }
+
+    return {
+        getUserToken: function () {
+            return _retrieveValue("userToken");
+        },
+        setUserToken: function (value) {
+            return _saveValue("userToken", value);
+        },
+
+        get: _retrieveValue,
+        set: _saveValue,
     }
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('smms.controllers', ['smms.calls.services'])
 
-.controller('SmmsCtrl', function ($scope, $ionicModal, $timeout, LoginService) {
+.controller('SmmsCtrl', function ($scope, $ionicModal, $ionicPopup, $timeout, LoginService, SettingsService) {
 
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
@@ -8,6 +8,15 @@ angular.module('smms.controllers', ['smms.calls.services'])
     // listen for the $ionicView.enter event:
     //$scope.$on('$ionicView.enter', function(e) {
     //});
+    // An alert dialog
+    $scope.showAlert = function (title, message) {
+        var alertPopup = $ionicPopup.alert({
+            title: title,
+            template: message
+        });
+
+        alertPopup.then(function (res) {});
+    };
 
     // Form data for the login modal
     $scope.loginData = {};
@@ -17,6 +26,9 @@ angular.module('smms.controllers', ['smms.calls.services'])
         scope: $scope
     }).then(function (modal) {
         $scope.modal = modal;
+        if (SettingsService.getUserToken() == null) {
+            $scope.login();
+        };
     });
 
     // Triggered in the login modal to close it
@@ -38,42 +50,84 @@ angular.module('smms.controllers', ['smms.calls.services'])
         var hash = shaObj.getHash("HEX");
         user.password = hash;
         LoginService.getRole(user.username).success(function (data, status, headers, config) {
-            console.log(data);
             user.groups = [{
                 usergroup: data
             }];
             LoginService.getUser(user).success(function (data, status, headers, config) {
-                console.log(data);
+                $scope.showAlert('Logged-in succesfully!');
+                SettingsService.setUserToken(data.password);
+                $scope.closeLogin();
             }).error(function (data, status, headers, config) {
+                $scope.showAlert('Error getting user');
                 console.log(data);
+                console.log(headers);
+                console.log(status);
             });
         }).error(function (data, status, headers, config) {
+            $scope.showAlert('Error getting user role');
             console.log(data);
             console.log(headers);
             console.log(status);
         });
-
-        // Simulate a login delay. Remove this and replace with your login
-        // code if using a login system
-        //        $timeout(function () {
-        //            $scope.closeLogin();
-        //        }, 1000);
     };
+
+
+
 })
 
 .controller('CallsCtrl', function ($scope, CallService) {
-    $scope.ctrlVars = {};
-    $scope.ctrlVars.showLoadingIcon = true;
-    $scope.calls = [];
-    CallService.getCalls().success(function (data, status, headers, config) {
 
-        $scope.calls = data;
-        $scope.ctrlVars.showLoadingIcon = false;
-    }).error(function (data, status, headers, config) {
-
-        $scope.ctrlVars.showLoadingIcon = false;
-        alert(headers);
+    $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+        if (toState.url == "/calls") {
+            $scope.ctrlVars.showLoadingIcon = true;
+            $scope.doRefresh();
+        }
     });
+    $scope.calls = {};
+    $scope.ctrlVars = {};
+    $scope.ctrlVars.showLoadingIcon = false;
+    $scope.ctrlVars.errorList = [];
+
+    $scope.doRefresh = function () {
+        $scope.ctrlVars.errorList = [];
+
+        CallService.getCalls().success(function (data, status, headers, config) {
+            $scope.calls = data;
+            $scope.ctrlVars.showLoadingIcon = false;
+            $scope.$broadcast('scroll.refreshComplete');
+            $scope.shownCalls = [];
+            currentStart = 0;
+            $scope.addItems();
+        }).error(function (data, status, headers, config) {
+            $scope.shownCalls = [];
+            currentStart = 0;
+            $scope.ctrlVars.errorList.push({
+                message: "It wasn't possible to retrieve the calls"
+            });
+            $scope.ctrlVars.showLoadingIcon = false;
+            $scope.$broadcast('scroll.refreshComplete');
+            $scope.ctrlVars.showLoadingIcon = false;
+        });
+    };
+
+    $scope.shownCalls = [];
+    var currentStart = 0;
+    /* Add more itens to be displayed on the page */
+    $scope.addItems = function () {
+        var i;
+        for (i = currentStart; i < currentStart + 20 && i < $scope.calls.length; i++) {
+
+            $scope.shownCalls.push($scope.calls[i]);
+        }
+        currentStart += i;
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+    };
+    /* End addItens */
+
+    $scope.moreDataCanBeLoaded = function () {
+        return $scope.calls.length > currentStart;
+    }
+
 })
 
 .controller('CallCtrl', function ($scope, $stateParams, CallService) {
